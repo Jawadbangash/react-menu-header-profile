4b919908cda02de1a66de3874f42cd1909dabaa9
Jawad <jawadbanga@gmail.com>
2021-04-22 22:11:00 -0700

    furter development on header menu feature

--- src/headerComponent.jsx
+++ src/headerComponent.jsx
@@ -1,15 +1,54 @@
 import React from 'react'
-import './headerComp.css';
+import './headerComp.css'
+import imgBurger from './Hamburger_icon.svg.png'
+
+const arrMenuItems =[{name: 'apple', image: 'img'}]
 
 class TopHeader extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor (props) {
+        super (props)
+        this.state ={
+            showMenu: true
+        }
+    }
+
+    menuButtonHandler (event, a, b, c) {
+        console.log('triggered', event, a, b, c)
+        this.setState({
+            showMenu: !this.state.showMenu
+        })
+    }
+
+    menuItemClickHandler (event, a, b, c) {
+        console.log(event, a, b, c)
+        // this.setState({showMenu: false})
     }
 
     render () {
         return (
-            <div className='header-website' >
-                <button className='button-menu'></button>
+            <div className='header-top-container'>       
+                <div className='header-website' >
+                    <button className='button-menu' 
+                        onClick={(event, a, b, c) => this.menuButtonHandler(event, a, b, c)}
+                    >
+                        <img src={imgBurger} width='25vw' height='25vh'/>
+                    </button>
+                </div>
+                {
+                    this.state.showMenu ? (
+                        <div className='menu-bar' >
+                            <div id='menu-bar-item' onClick={(event, a, b, c) => this.menuItemClickHandler(event, a, b, c)}>
+                                <p id='text-in-menuitem'>Sam </p>
+                            </div>                            
+                            <div id='menu-bar-item'>
+                                <p id='text-in-menuitem'>Sam sdafsdfafdawfam sdfasfawfasdf slsdkjflkasdjflkjasdf </p>
+                            </div>                            
+                            <div id='menu-bar-item'>
+                                <p id='text-in-menuitem'>Sam </p>
+                            </div>
+                        </div>
+                    ) : null 
+                }
             </div>
         )
     }
